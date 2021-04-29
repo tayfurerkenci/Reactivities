4d3e2c86bd699e8f82b859dfa33cd6b992396546
Tayfur Erkenci <terkenci@gmail.com>
2021-04-30 02:59:06 +0300

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,6 +19,16 @@ export default class activityStore {
             Date.parse(a.date) - Date.parse(b.date));
     }
 
+    get groupedActivities(){
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+        )
+    }
+
     loadActivities = async () => {
         this.loadingInitial=true;
         // synchronous code inside try catch block!
